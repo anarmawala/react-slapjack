f423dcc614c974d56a0555f4ab01817c3bdd1bde
anarmawala <arshadn00@gmail.com>
2019-04-26 16:41:14 -0500

    Started the classes

---
+++ server/Slapjack/Player/index.js
@@ -0,0 +1,10 @@
+/**
+ *
+ */
+class Player {
+  /**
+   *
+   */
+}
+
+export default Player;
---
+++ server/Slapjack/index.js
@@ -0,0 +1,32 @@
+/**
+ * Slapjack
+ *
+ * Holds all the logic for the game
+ */
+class Slapjack {
+  /**
+   * Initializes the variable
+   */
+  constructor() {
+    this.players = [];
+    this.table = [];
+  }
+
+  /**
+   * players
+   *
+   * @return {Player[]} the array of players
+   */
+  get players() {
+    return this.players;
+  }
+
+  /**
+   * @return {number} number of players in the game
+   */
+  numPlayers() {
+    return this.players.size();
+  }
+}
+
+export default Slapjack;
