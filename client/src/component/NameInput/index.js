c09a3f375252788e8652f2b299c2d24fcb76453a
anarmawala <arshadn00@gmail.com>
2019-05-01 20:30:05 -0500

    Added pattern matching on inputs and validity messages

--- client/src/component/NameInput/index.js
+++ client/src/component/NameInput/index.js
@@ -22,7 +22,7 @@ const styles = {
 };
 
 const NameInput = (props) => {
-  const [user, setUser] = useState('');
+  const [user, setUser] = useState('Anonymous');
   const [IP, setIP] = useState('127.0.0.1');
   const [port, setPort] = useState('8080');
 
@@ -48,6 +48,16 @@ const NameInput = (props) => {
         onChange={(e) => {
           setIP(e.target.value);
         }}
+        pattern="\b(?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9]))\b"
+        onInvalid={(e) => {
+          e.target.setCustomValidity('');
+          if (!e.target.validity.valid) {
+            e.target.setCustomValidity('Please enter a valid IP address');
+          }
+        }}
+        onInput={(e) => {
+          e.target.setCustomValidity('');
+        }}
       />
 
       <input
@@ -59,6 +69,16 @@ const NameInput = (props) => {
         onChange={(e) => {
           setPort(e.target.value);
         }}
+        pattern="^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$"
+        onInvalid={(e) => {
+          e.target.setCustomValidity('');
+          if (!e.target.validity.valid) {
+            e.target.setCustomValidity('Please enter a valid port');
+          }
+        }}
+        onInput={(e) => {
+          e.target.setCustomValidity('');
+        }}
       />
 
       <input
@@ -69,6 +89,18 @@ const NameInput = (props) => {
         style={styles.input}
         onChange={(e) => {
           setUser(e.target.value);
+        }}
+        pattern="[a-zA-Z][a-zA-Z0-9-_]{0,20}"
+        onInvalid={(e) => {
+          e.target.setCustomValidity('');
+          if (!e.target.validity.valid) {
+            e.target.setCustomValidity(
+              'Names can be up to 20 characters long and cannot have special characters'
+            );
+          }
+        }}
+        onInput={(e) => {
+          e.target.setCustomValidity('');
         }}
       />
       <br />
