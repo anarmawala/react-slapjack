5b61aa8de417969572fcb5e26e8dac474fa1d2ac
Clark Chen <9372896+clarkchentw@users.noreply.github.com>
2019-05-06 03:23:11 +0800

    recover slapjack.js and player.js

--- server/Slapjack/Player/player.js
+++ server/Slapjack/Player/player.js
@@ -1,3 +1,6 @@
+/*
+*	Need to add class for cards 
+*/
 /**
  * Player
  *
@@ -8,17 +11,56 @@ class Player {
 	/**
     * Initializes the variables
     */
-	Player(id) {
-		this.id = id;	  
-	}
-  
-	set name(name) {
-		this.userName = name;
+	Player(id, userName) {
+		this.id = id;
+		this.playerCards = [];	
+		this.userName = userName;
 	}
 	
-	get name() {
-		return this.userName;
+	/*
+	* input Player object, and returns id
+	*/
+	static getId(player) {
+		return player.id;
 	}
+	
+	/*
+	* input Player object, and returns user name
+	*/
+	static getName(player) {
+		return player.userName;
+	}
+	
+	/*
+	* input Player object, and returns playing cards
+	*/
+	static getCards(player){
+		return player.playerCards;
+	}
+	
+	/**
+	*
+	* @return {int} number of cards in player's hand
+	*/
+	numOfCards() {
+		return this.playerCards.size();
+	}
+	
+	/**
+    *
+    * @param {*} card that will be added to the player's hand
+    */
+    addToHand(card) {
+		this.playerCards.push(card);
+    }
+	
+	/**
+    *
+    * @return {Card} the first card in players hand
+    */
+    removeFromHand() {
+		return this.playerCards.pop();
+    }
 }
 
 
