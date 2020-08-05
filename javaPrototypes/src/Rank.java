782cf50c0917862e630be89617ffe0a579c21989
Arshad Narmawala <arshadn00@gmail.com>
2020-08-05 16:17:10 -0500

    🔥 remove/Remove Old Java code (#25)

--- javaPrototypes/src/Rank.java
+++
@@ -1,3 +0,0 @@
-enum Rank {
-    Ace, King, Queen, Jack, Ten, Nine, Eight, Seven, Six, Five, Four, Three, Two
-}
--- javaPrototypes/src/Suite.java
+++
@@ -1,6 +0,0 @@
-public enum Suite {
-    Diamonds,
-    Hearts,
-    Clubs,
-    Spades
-}
